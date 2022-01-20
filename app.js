6a70ca9b3624d8b650fdefd2ef45f1920699a899
עמית בריקמן <ichsunnosaf@gmail.com>
2022-01-20 11:56:14 +0200

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const cors = require("cors");
+app.use(cors());
+
+let status = "לא";
+
+app.get("/status", (req, res) => {
+  res.send({
+    status: getStatus(),
+  });
+});
+
+app.post("/status/:new", (req, res) => {
+  changeStatus(req.params.new);
+  res.status(200).send();
+});
+
+const changeStatus = (newVal) => {
+  console.log("new status is " + newVal);
+  status = newVal;
+};
+
+const getStatus = () => {
+  return status;
+};
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
